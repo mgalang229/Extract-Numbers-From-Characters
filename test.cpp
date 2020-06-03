2205409fb9f60e5a0719a2e0b48c34abee97028b
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2020-06-03 21:02:00 +0800

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	char *s;
+	for(int i=0; i<3; i++)
+		cin >> s[i];
+	int t=atoi(s);
+	cout << t+2 << "\n";
+	return 0;
+}
